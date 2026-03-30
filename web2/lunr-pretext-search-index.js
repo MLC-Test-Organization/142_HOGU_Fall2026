c08924836be40edac5e3ca3aa8a4b54f5b2d07ff
Christy Nakad <christynakad@tamu.edu>
2026-03-30 18:25:16 +0000

    Latest build deployed.

---
+++ web2/lunr-pretext-search-index.js
@@ -0,0 +1,176 @@
+var ptx_lunr_search_style = "textbook";
+var ptx_lunr_docs = [
+{
+  "id": "session2wks",
+  "level": "1",
+  "url": "#session2wks",
+  "type": "Article",
+  "number": "",
+  "title": "MATH 142 Hands-On, Grades Up",
+  "body": " MATH 142 Hands-On, Grades Up   For workspace between exercises, print this worksheet or download to your device by clicking the printer icon.     Session #2 (Sections 1.3-1.4)   Directions: Answer each of the following as best you can. Feel free to discuss your thoughts about the questions with some peers around you. For additional practice problems and explanations, refer to the Math 142 textbook which is free and can be downloaded as a PDF at Texas A&M The OAKTrust Digital Repository .   Warm Up     Given and , what do you then know about any horizontal asymptotes of ?      Given the following information about , what do you know about the horizontal and vertical asymptotes?         Determine each of the following limits. Input the functions into your graphing calculator if you prefer a graphical view to find the limits.   (a)    (b)    (c)            (d)    (e)    (f)       (g)    (h)    (i)                Examine the limits above, group them together by finding patterns or common characteristics, then fill in the blanks below. Some are already filled in for you.   Given where is any real number and then and .    Given where and then and .    Given where and then and .          Let Us Get Our Hands On     Find any (a) horizontal asymptotes, (b) the coordinates of any holes, and (c) vertical asymptotes of the function. If there are no horizontal asymptotes, describe the end behavior using limit notation.                   Given , follow the steps below to find the horizontal asymptotes of , if any exist.   Set up but do not yet solve the two limits that are needed to determine the horizontal asymptotes.    Fill in the blanks with either positive or negative .    If , we divide all terms by the in the denominator that has the most power.      If , we divide all terms by the in the denominator that has the most power.        Apply the techniques from part(b) to compute the limits in part(a) and state the horizontal asymptotes of . If there are none, describe the end behavior using limit notation. It may help to refer to problem 4 from the “Getting Warmed Up and In the Right Mindset” section.           Below is an outline of the Definition of Continuity at a Point. Fill in the blanks below to correctly complete the text in the definition. The Math 142 textbook has some very good references to help better understand continuity. See pages 124-131 for definitions, examples, and step-by-step instructions. A function is continuous at a point where if and only if the following three conditions are satisfied:  is defined.  exists.           If you know and , can you correctly conclude that continuous at ? Why or why not?      Use the graph of to determine if is continuous at .    Is defined? If so, find the value.    Does exist? If so, find the value.    Does ?    Is is continuous at ? If not, at what condition (I, II, or III) does it fail first?           Use the graph of to determine if is continuous at .    Is defined? If so, find the value.    Does exist? If so, find the value.    Does ?    Is is continuous at ? If not, at what condition (I, II, or III) does it fail first?         Use the graph of to determine if is continuous at .    Is defined? If so, find the value.    Does exist? If so, find the value.    Does ?    Is is continuous at ? If not, at what condition (I, II, or III) does it fail first?           Use the graph of to determine if is continuous at .    Is defined? If so, find the value.    Does exist? If so, find the value.    Does ?    Is is continuous at ? If not, at what condition (I, II, or III) does it fail first?         Use the graph of to determine if is continuous at .    Is defined? If so, find the value.    Does exist? If so, find the value.    Does ?    Is is continuous at ? If not, at what condition (I, II, or III) does it fail first?           Determine the intervals of continuity for each function below. Page 126 of your textbook has a list of domain restrictions that will help find the intervals of continuity for functions in symbolic form.                       Given           Now Grades Go Up     Problem 1 of final section      Problem 2      Problem 3      "
+},
+{
+  "id": "session2wks-3-2-3",
+  "level": "2",
+  "url": "#session2wks-3-2-3",
+  "type": "Worksheet Exercise",
+  "number": "1",
+  "title": "",
+  "body": "  Given and , what do you then know about any horizontal asymptotes of ?   "
+},
+{
+  "id": "session2wks-3-2-4",
+  "level": "2",
+  "url": "#session2wks-3-2-4",
+  "type": "Worksheet Exercise",
+  "number": "2",
+  "title": "",
+  "body": "  Given the following information about , what do you know about the horizontal and vertical asymptotes?    "
+},
+{
+  "id": "session2wks-3-3-1",
+  "level": "2",
+  "url": "#session2wks-3-3-1",
+  "type": "Worksheet Exercise",
+  "number": "3",
+  "title": "",
+  "body": "  Determine each of the following limits. Input the functions into your graphing calculator if you prefer a graphical view to find the limits.   (a)    (b)    (c)            (d)    (e)    (f)       (g)    (h)    (i)             "
+},
+{
+  "id": "session2wks-3-3-2",
+  "level": "2",
+  "url": "#session2wks-3-3-2",
+  "type": "Worksheet Exercise",
+  "number": "4",
+  "title": "",
+  "body": "  Examine the limits above, group them together by finding patterns or common characteristics, then fill in the blanks below. Some are already filled in for you.   Given where is any real number and then and .    Given where and then and .    Given where and then and .      "
+},
+{
+  "id": "session2wks-3-4-2",
+  "level": "2",
+  "url": "#session2wks-3-4-2",
+  "type": "Worksheet Exercise",
+  "number": "1",
+  "title": "",
+  "body": "  Find any (a) horizontal asymptotes, (b) the coordinates of any holes, and (c) vertical asymptotes of the function. If there are no horizontal asymptotes, describe the end behavior using limit notation.              "
+},
+{
+  "id": "session2wks-3-5-1",
+  "level": "2",
+  "url": "#session2wks-3-5-1",
+  "type": "Worksheet Exercise",
+  "number": "2",
+  "title": "",
+  "body": "  Given , follow the steps below to find the horizontal asymptotes of , if any exist.   Set up but do not yet solve the two limits that are needed to determine the horizontal asymptotes.    Fill in the blanks with either positive or negative .    If , we divide all terms by the in the denominator that has the most power.      If , we divide all terms by the in the denominator that has the most power.        Apply the techniques from part(b) to compute the limits in part(a) and state the horizontal asymptotes of . If there are none, describe the end behavior using limit notation. It may help to refer to problem 4 from the “Getting Warmed Up and In the Right Mindset” section.      "
+},
+{
+  "id": "session2wks-3-6-1",
+  "level": "2",
+  "url": "#session2wks-3-6-1",
+  "type": "Worksheet Exercise",
+  "number": "3",
+  "title": "",
+  "body": "  Below is an outline of the Definition of Continuity at a Point. Fill in the blanks below to correctly complete the text in the definition. The Math 142 textbook has some very good references to help better understand continuity. See pages 124-131 for definitions, examples, and step-by-step instructions. A function is continuous at a point where if and only if the following three conditions are satisfied:  is defined.  exists.        "
+},
+{
+  "id": "session2wks-3-6-2",
+  "level": "2",
+  "url": "#session2wks-3-6-2",
+  "type": "Worksheet Exercise",
+  "number": "4",
+  "title": "",
+  "body": "  If you know and , can you correctly conclude that continuous at ? Why or why not?   "
+},
+{
+  "id": "session2wks-3-6-3",
+  "level": "2",
+  "url": "#session2wks-3-6-3",
+  "type": "Worksheet Exercise",
+  "number": "5",
+  "title": "",
+  "body": "  Use the graph of to determine if is continuous at .    Is defined? If so, find the value.    Does exist? If so, find the value.    Does ?    Is is continuous at ? If not, at what condition (I, II, or III) does it fail first?      "
+},
+{
+  "id": "session2wks-3-7-1",
+  "level": "2",
+  "url": "#session2wks-3-7-1",
+  "type": "Worksheet Exercise",
+  "number": "6",
+  "title": "",
+  "body": "  Use the graph of to determine if is continuous at .    Is defined? If so, find the value.    Does exist? If so, find the value.    Does ?    Is is continuous at ? If not, at what condition (I, II, or III) does it fail first?      "
+},
+{
+  "id": "session2wks-3-7-2",
+  "level": "2",
+  "url": "#session2wks-3-7-2",
+  "type": "Worksheet Exercise",
+  "number": "7",
+  "title": "",
+  "body": "  Use the graph of to determine if is continuous at .    Is defined? If so, find the value.    Does exist? If so, find the value.    Does ?    Is is continuous at ? If not, at what condition (I, II, or III) does it fail first?      "
+},
+{
+  "id": "session2wks-3-8-1",
+  "level": "2",
+  "url": "#session2wks-3-8-1",
+  "type": "Worksheet Exercise",
+  "number": "8",
+  "title": "",
+  "body": "  Use the graph of to determine if is continuous at .    Is defined? If so, find the value.    Does exist? If so, find the value.    Does ?    Is is continuous at ? If not, at what condition (I, II, or III) does it fail first?      "
+},
+{
+  "id": "session2wks-3-8-2",
+  "level": "2",
+  "url": "#session2wks-3-8-2",
+  "type": "Worksheet Exercise",
+  "number": "9",
+  "title": "",
+  "body": "  Use the graph of to determine if is continuous at .    Is defined? If so, find the value.    Does exist? If so, find the value.    Does ?    Is is continuous at ? If not, at what condition (I, II, or III) does it fail first?      "
+},
+{
+  "id": "session2wks-3-9-1",
+  "level": "2",
+  "url": "#session2wks-3-9-1",
+  "type": "Worksheet Exercise",
+  "number": "10",
+  "title": "",
+  "body": "  Determine the intervals of continuity for each function below. Page 126 of your textbook has a list of domain restrictions that will help find the intervals of continuity for functions in symbolic form.                       Given       "
+},
+{
+  "id": "session2wks-3-10-2",
+  "level": "2",
+  "url": "#session2wks-3-10-2",
+  "type": "Worksheet Exercise",
+  "number": "1",
+  "title": "",
+  "body": "  Problem 1 of final section   "
+},
+{
+  "id": "session2wks-3-10-3",
+  "level": "2",
+  "url": "#session2wks-3-10-3",
+  "type": "Worksheet Exercise",
+  "number": "2",
+  "title": "",
+  "body": "  Problem 2   "
+},
+{
+  "id": "session2wks-3-10-4",
+  "level": "2",
+  "url": "#session2wks-3-10-4",
+  "type": "Worksheet Exercise",
+  "number": "3",
+  "title": "",
+  "body": "  Problem 3   "
+}
+]
+
+var ptx_lunr_idx = lunr(function () {
+  this.ref('id')
+  this.field('title')
+  this.field('body')
+  this.metadataWhitelist = ['position']
+
+  ptx_lunr_docs.forEach(function (doc) {
+    this.add(doc)
+  }, this)
+})
