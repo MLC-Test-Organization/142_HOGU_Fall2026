01c6732e97e4d2a4850d80feb1df4c9a353a0add
Christy Nakad <christynakad@tamu.edu>
2026-04-06 14:53:38 +0000

    Latest build deployed.

---
+++ web3/lunr-pretext-search-index.js
@@ -0,0 +1,203 @@
+var ptx_lunr_search_style = "textbook";
+var ptx_lunr_docs = [
+{
+  "id": "session3wks",
+  "level": "1",
+  "url": "#session3wks",
+  "type": "Article",
+  "number": "",
+  "title": "MATH 142 Hands-On, Grades Up",
+  "body": " MATH 142 Hands-On, Grades Up   For workspace between exercises, print this worksheet or download to your device by clicking the printer icon.     Session #3 (Sections 1.1-1.4, 2.1)  Directions: Answer each of the following as best you can. Feel free to discuss your thoughts about the questions with some peers around you. For additional practice problems and explanations, refer to the Math 142 textbook which is free and can be downloaded as a PDF at Texas A&M The OAKTrust Digital Repository .    Getting Warmed Up and in the Right Mindset     State the formula for finding the average rate of change of a function on the interval .  This is also known as the slope of the line.      Find the average rate of change of on the interval . Draw the line that represents this average rate of change on the graph.   This is some alt text.       State the formula for the difference quotient of a function at .      Compute the difference quotient of at with . (Round your answer to four decimal places, if needed).       Let Us Get Our Hands On     The cost of a company to produce products is given by dollars.   Calculate the average rate of change of cost on each of the following intervals.                            Use part (a) to approximate the instantaneous rate of change of cost when products are produced. Interpret your results.         Suppose we want to find the instantaneous rate of change of at :   Circle the correct formula for calculating the instantaneous rate of change of at :     Follow the steps below to compute .   Calculate and combine like terms.    Calculate and combine like terms.    Calculate the difference quotient and simplify to get rid of on the bottom.    Take the limit of your simplified expression in iii. Label the resulting value as .              In #2 we found that the instantaneous rate of change of at is . Use this to find the equation of the line tangent to at .      Find the instantaneous rate of change of at . Hint: Follow the same steps as in #2. Multiply by the conjugate in step iii and simplify. If you don't do this, then taking the limit in step iv. gives you the indeterminate form which we can’t work with.         Find the equation of the line tangent to at .      Use the graph of below to answer questions #6-7.   This is some alt text.     Find the following limits.                                      Find all values where is discontinuous. For each value, state the condition (I, II, or III) within the definition of continuity that fails first.      Estimate the following limits numerically by building a table with at least 3 -values approaching the given -value. If the limit does not exist, state so and and use limit notation to describe any infinite behavior. Round to four decimal places if necessary.          Hint: Create two tables.           Now Grades Go Up     Determine whether exists. If it does exist, find the limit. If it does not exist, describe the behavior of the function at .   The limit exists, and it equals .    The limit exists, and it equals .    The limit exists, and it equals .    The limit does not exist, but and .    The limit does not exist, but and .         Find .             DNE                   Find the horizontal asymptotes for     does not have any horizontal asymptotes.     has two horizontal asymptotes at and .     has only one horizontal asymptote at .     has only one horizontal asymptote at .     has only one horizontal asymptote at .         Find the intervals where is continuous.                                  Suppose for some function , you know . What can you conclude happens at ?    has a vertical asymptote at .     has a horizontal asymptote at .     is continuous at .     has a hole at .              Given , find all vertical asymptotes for .    , , and           and      .     ,          Given , find the values where all holes occur for .    and      , , and           and     does not have any holes.         Determine whether each statement is true or false.   True\/False: If is a rational function with , then it must be that .    True\/False: If is a rational function with , then it must also be that .    True\/False: If , then it must also be that .         "
+},
+{
+  "id": "session3wks-3-3-2",
+  "level": "2",
+  "url": "#session3wks-3-3-2",
+  "type": "Worksheet Exercise",
+  "number": "1",
+  "title": "",
+  "body": "  State the formula for finding the average rate of change of a function on the interval .  This is also known as the slope of the line.   "
+},
+{
+  "id": "session3wks-3-3-3",
+  "level": "2",
+  "url": "#session3wks-3-3-3",
+  "type": "Worksheet Exercise",
+  "number": "2",
+  "title": "",
+  "body": "  Find the average rate of change of on the interval . Draw the line that represents this average rate of change on the graph.   This is some alt text.    "
+},
+{
+  "id": "session3wks-3-3-4",
+  "level": "2",
+  "url": "#session3wks-3-3-4",
+  "type": "Worksheet Exercise",
+  "number": "3",
+  "title": "",
+  "body": "  State the formula for the difference quotient of a function at .   "
+},
+{
+  "id": "session3wks-3-3-5",
+  "level": "2",
+  "url": "#session3wks-3-3-5",
+  "type": "Worksheet Exercise",
+  "number": "4",
+  "title": "",
+  "body": "  Compute the difference quotient of at with . (Round your answer to four decimal places, if needed).   "
+},
+{
+  "id": "session3wks-3-4-2",
+  "level": "2",
+  "url": "#session3wks-3-4-2",
+  "type": "Worksheet Exercise",
+  "number": "1",
+  "title": "",
+  "body": "  The cost of a company to produce products is given by dollars.   Calculate the average rate of change of cost on each of the following intervals.                            Use part (a) to approximate the instantaneous rate of change of cost when products are produced. Interpret your results.      "
+},
+{
+  "id": "session3wks-3-4-3",
+  "level": "2",
+  "url": "#session3wks-3-4-3",
+  "type": "Worksheet Exercise",
+  "number": "2",
+  "title": "",
+  "body": "  Suppose we want to find the instantaneous rate of change of at :   Circle the correct formula for calculating the instantaneous rate of change of at :     Follow the steps below to compute .   Calculate and combine like terms.    Calculate and combine like terms.    Calculate the difference quotient and simplify to get rid of on the bottom.    Take the limit of your simplified expression in iii. Label the resulting value as .         "
+},
+{
+  "id": "session3wks-3-5-1",
+  "level": "2",
+  "url": "#session3wks-3-5-1",
+  "type": "Worksheet Exercise",
+  "number": "3",
+  "title": "",
+  "body": "  In #2 we found that the instantaneous rate of change of at is . Use this to find the equation of the line tangent to at .   "
+},
+{
+  "id": "session3wks-3-5-2",
+  "level": "2",
+  "url": "#session3wks-3-5-2",
+  "type": "Worksheet Exercise",
+  "number": "4",
+  "title": "",
+  "body": "  Find the instantaneous rate of change of at . Hint: Follow the same steps as in #2. Multiply by the conjugate in step iii and simplify. If you don't do this, then taking the limit in step iv. gives you the indeterminate form which we can’t work with.    "
+},
+{
+  "id": "session3wks-3-6-1",
+  "level": "2",
+  "url": "#session3wks-3-6-1",
+  "type": "Worksheet Exercise",
+  "number": "5",
+  "title": "",
+  "body": "  Find the equation of the line tangent to at .   "
+},
+{
+  "id": "session3wks-3-7-3",
+  "level": "2",
+  "url": "#session3wks-3-7-3",
+  "type": "Worksheet Exercise",
+  "number": "6",
+  "title": "",
+  "body": "  Find the following limits.                                   "
+},
+{
+  "id": "session3wks-3-7-4",
+  "level": "2",
+  "url": "#session3wks-3-7-4",
+  "type": "Worksheet Exercise",
+  "number": "7",
+  "title": "",
+  "body": "  Find all values where is discontinuous. For each value, state the condition (I, II, or III) within the definition of continuity that fails first.   "
+},
+{
+  "id": "session3wks-3-7-5",
+  "level": "2",
+  "url": "#session3wks-3-7-5",
+  "type": "Worksheet Exercise",
+  "number": "8",
+  "title": "",
+  "body": "  Estimate the following limits numerically by building a table with at least 3 -values approaching the given -value. If the limit does not exist, state so and and use limit notation to describe any infinite behavior. Round to four decimal places if necessary.          Hint: Create two tables.       "
+},
+{
+  "id": "session3wks-3-8-2",
+  "level": "2",
+  "url": "#session3wks-3-8-2",
+  "type": "Worksheet Exercise",
+  "number": "1",
+  "title": "",
+  "body": "  Determine whether exists. If it does exist, find the limit. If it does not exist, describe the behavior of the function at .   The limit exists, and it equals .    The limit exists, and it equals .    The limit exists, and it equals .    The limit does not exist, but and .    The limit does not exist, but and .      "
+},
+{
+  "id": "session3wks-3-8-3",
+  "level": "2",
+  "url": "#session3wks-3-8-3",
+  "type": "Worksheet Exercise",
+  "number": "2",
+  "title": "",
+  "body": "  Find .             DNE                "
+},
+{
+  "id": "session3wks-3-8-4",
+  "level": "2",
+  "url": "#session3wks-3-8-4",
+  "type": "Worksheet Exercise",
+  "number": "3",
+  "title": "",
+  "body": "  Find the horizontal asymptotes for     does not have any horizontal asymptotes.     has two horizontal asymptotes at and .     has only one horizontal asymptote at .     has only one horizontal asymptote at .     has only one horizontal asymptote at .      "
+},
+{
+  "id": "session3wks-3-8-5",
+  "level": "2",
+  "url": "#session3wks-3-8-5",
+  "type": "Worksheet Exercise",
+  "number": "4",
+  "title": "",
+  "body": "  Find the intervals where is continuous.                               "
+},
+{
+  "id": "session3wks-3-8-6",
+  "level": "2",
+  "url": "#session3wks-3-8-6",
+  "type": "Worksheet Exercise",
+  "number": "5",
+  "title": "",
+  "body": "  Suppose for some function , you know . What can you conclude happens at ?    has a vertical asymptote at .     has a horizontal asymptote at .     is continuous at .     has a hole at .           "
+},
+{
+  "id": "session3wks-3-8-7",
+  "level": "2",
+  "url": "#session3wks-3-8-7",
+  "type": "Worksheet Exercise",
+  "number": "6",
+  "title": "",
+  "body": "  Given , find all vertical asymptotes for .    , , and           and      .     ,       "
+},
+{
+  "id": "session3wks-3-8-8",
+  "level": "2",
+  "url": "#session3wks-3-8-8",
+  "type": "Worksheet Exercise",
+  "number": "7",
+  "title": "",
+  "body": "  Given , find the values where all holes occur for .    and      , , and           and     does not have any holes.      "
+},
+{
+  "id": "session3wks-3-8-9",
+  "level": "2",
+  "url": "#session3wks-3-8-9",
+  "type": "Worksheet Exercise",
+  "number": "8",
+  "title": "",
+  "body": "  Determine whether each statement is true or false.   True\/False: If is a rational function with , then it must be that .    True\/False: If is a rational function with , then it must also be that .    True\/False: If , then it must also be that .      "
+}
+]
+
+var ptx_lunr_idx = lunr(function () {
+  this.ref('id')
+  this.field('title')
+  this.field('body')
+  this.metadataWhitelist = ['position']
+
+  ptx_lunr_docs.forEach(function (doc) {
+    this.add(doc)
+  }, this)
+})
